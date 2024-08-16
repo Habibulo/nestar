a3a865dfe6211acfa8098e2d1aa7c5aa63223b90
Habibullo <hhalimjonov1997@gmail.com>
2024-08-16 16:34:32 +0900

    feat: integration GraphQL Module and build first GraphQL API

---
+++ documentation.ts
@@ -0,0 +1,16 @@
+/*
+    95_96_97_Dars 
+    Darsimiz Rejasi: 
+        95 => 
+        - NestJS ozi nima?
+        - NestJS da muhim orinni egallagan NestJS Ingredientlari haqida ga[lashamiz
+        - NestJS markaziy Module Decorator tushunchasi va uning tarkibiy qismlarini tahlil etamiz
+        - NestJS da Rest API bolmish Zoo loyihasini birga tashkil etamiz
+=========================================================================
+        96 => 
+        - 
+=========================================================================
+        97 => 
+        - Loyihamizga GraphQL intefratsiyasini amalga oshiramiz va ilk GraphQL API requestlarimizni hosil qilamiz
+        - Loyihamizning standardlarini birga shakllantiramiz
+*/
