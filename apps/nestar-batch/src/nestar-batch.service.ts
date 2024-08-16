397c37f320f002035a78ef9d208be957d2fb2a17
Habibullo <hhalimjonov1997@gmail.com>
2024-08-16 16:04:19 +0900

    fix: installation packages&&modify format standards

--- apps/nestar-batch/src/nestar-batch.service.ts
+++ apps/nestar-batch/src/nestar-batch.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class NestarBatchService {
-  getHello(): string {
-    return 'Welcome to Batch Server';
-  }
+	getHello(): string {
+		return 'Welcome to Batch Server';
+	}
 }
