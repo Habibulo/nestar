fc5b11470180673f397cb724bd1a1debde5840be
Habibullo <hhalimjonov1997@gmail.com>
2024-08-18 19:10:01 +0900

    feat: create member reslover&service Component module

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -1,4 +1,14 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
+import { MemberResolver } from './member.resolver';
+import { MemberService } from './member.service';
+import MemberSchema from '../../schemas/Member.model';
 
-@Module({})
+@Module({
+	//MemberSchema - bu object
+	//Schema - class
+	//'Member'-schema modeli xosil bolyapti
+	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }])],
+	providers: [MemberResolver, MemberService],
+})
 export class MemberModule {}
