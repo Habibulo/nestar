5c65c493b5066afb04ddb897a96d306fba202e5e
Habibullo <hhalimjonov1997@gmail.com>
2024-09-06 17:16:03 +0900

    feat: integrate comment module related DTOs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -28,6 +28,8 @@ export const availablePropertySorts = [
 	'propertyRank',
 	'propertyPrice',
 ];
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+export const availableCommentSorts = ['createdAt', 'updatedAt'];
 /* IMAGE CONFIGURATION (config.js) */
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
